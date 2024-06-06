3d36bafe9892596233a2c427c7587f865a4a7867
Andrew123-maker <142960669+Andrew123-maker@users.noreply.github.com>
2024-06-06 02:24:49 +0000

    modify allowed host

--- django_project/settings.py
+++ django_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-4ju2n@$f9d0c=h)_g0lbb%k9&@rf(xa$d$g$&5ri$uf)*gev^4
 DEBUG = True
 
 ALLOWED_HOSTS = [".replit.dev", ".replit.app",  "andrew123coder.pythonanywhere.com",
-"makerions123.pythonanywhere.com"]
+"makerion123coder.pythonanywhere.com"]
 CSRF_TRUSTED_ORIGINS = ["https://*.replit.dev", "https://*.replit.app"]
 
 # Application definition
